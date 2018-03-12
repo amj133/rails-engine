4985a3939ca58a2a6f1f07885fa66da4e46f41e0
Joe Jobes <jmrjobes@gmail.com>
2018-03-12 17:56:29 -0600

    Add invoice validations and relationships

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it {should validate_presence_of(:status)}
+    it {should validate_presence_of(:created_at)}
+    it {should validate_presence_of(:updated_at)}
+  end
+
+  describe "Relationships" do
+    it {should belong_to(:merchant)}
+    it {should belong_to(:customer)}
+  end
 end
