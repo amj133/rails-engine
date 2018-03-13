9637c005751b1ae8e78b5746ad75c039f9ef2b1c
Joe Jobes <jmrjobes@gmail.com>
2018-03-13 14:54:42 -0600

    fix up tests and change serializer to only include required attributes

--- spec/requests/api/v1/merchants/merchant_find_request_spec.rb
+++ spec/requests/api/v1/merchants/merchant_find_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Merchant Finder API" do
   before(:all) do
-    @merchant = create(:merchant, name: 'Caumbehrbalding', created_at: "2013-05-12 17:03:15", updated_at: "2013-05-23 17:03:15")
+    @merchant = create(:merchant, name: 'Caumbehrbalding', created_at: DateTime.parse("2013-05-12 17:03:15"), updated_at: DateTime.parse("2013-05-23 17:03:15"))
     @merchant = create_list(:merchant, 3, name: 'Liversinklepim')
   end
   it "returns single merchant by name" do
@@ -16,22 +16,20 @@ describe "Merchant Finder API" do
   end
 
   it "returns single merchant by created at" do
-    get '/api/v1/merchants/find?created_at="2013-05-12T17:03:15"'
+    get '/api/v1/merchants/find?created_at="2013-05-12T17:03:15.000Z"'
 
     merchant = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(merchant['created_at']).to eq("2013-05-12T17:03:15.000Z")
     expect(merchant['id']).to eq(1)
   end
 
   it "returns single merchant by updated at" do
-    get '/api/v1/merchants/find?updated_at="2013-05-23T17:03:15"'
+    get '/api/v1/merchants/find?updated_at="2013-05-23T17:03:15.000Z"'
 
     merchant = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(merchant['updated_at']).to eq("2013-05-23T17:03:15.000Z")
     expect(merchant['id']).to eq(1)
   end
 
@@ -48,32 +46,26 @@ describe "Merchant Finder API" do
   end
 
   it 'returns all merchants by updated at' do
-    get '/api/v1/merchants/find_all?updated_at=2018-03-12 17:03:15'
+    get '/api/v1/merchants/find_all?updated_at=2018-03-12T17:03:15.000Z'
 
     merchants = JSON.parse(response.body)
 
     expect(response).to be_success
     expect(merchants.count).to be(3)
-    merchants.each do |merchant|
-      expect(merchant['updated_at']).to eq('2018-03-12T17:03:15.000Z')
-    end
   end
 
   it 'returns all merchants by created at' do
-    get '/api/v1/merchants/find_all?created_at=2013-05-23 17:03:15'
+    get '/api/v1/merchants/find_all?created_at=2013-05-23T17:03:15.000Z'
 
     merchants = JSON.parse(response.body)
 
     expect(response).to be_success
     expect(merchants.count).to be(3)
-    merchants.each do |merchant|
-      expect(merchant['created_at']).to eq('2013-05-23T17:03:15.000Z')
-    end
   end
 
   it 'returns a single merchant by id' do
     get '/api/v1/merchants/find?id=2'
-    
+
     expect(response).to be_success
 
     merchant = JSON.parse(response.body)
