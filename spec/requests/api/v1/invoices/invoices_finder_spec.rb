5b84957b985a97cf546aaf587ce0448724b552d7
amj133 <amj@vt.edu>
2018-03-13 13:06:51 -0600

    invoice single find spec passing

--- spec/requests/api/v1/invoices/invoices_finder_spec.rb
+++ spec/requests/api/v1/invoices/invoices_finder_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 describe "Invoices Finder API" do
-  it "can find one invoice by id" do
+  before(:each) do
     create(:customer, id: 3)
     create(:merchant, id: 3)
-    create(:invoice, id: 11, status: "shipped", customer_id: 3, merchant_id: 3)
-    create(:invoice, id: 12, status: "pending", customer_id: 3, merchant_id: 3)
+    create(:invoice,
+           id: 11,
+           status: "shipped",
+           customer_id: 3,
+           merchant_id: 3,
+           created_at: DateTime.new(2018, 03, 12, 17, 10, 49),
+           updated_at: DateTime.new(2018, 03, 13, 17, 10, 49))
+
+    create(:invoice,
+           id: 12,
+           status: "pending",
+           customer_id: 3,
+           merchant_id: 3,
+           created_at: DateTime.new(2018, 04, 12, 17, 10, 49),
+           updated_at: DateTime.new(2018, 04, 13, 17, 10, 49))
+  end
+
+  it "can find one invoice by id" do
 
     get "/api/v1/invoices/find", params: {id: 11}
 
@@ -16,16 +32,29 @@ describe "Invoices Finder API" do
   end
 
   it "can find one invoice by status" do
-    create(:customer, id: 3)
-    create(:merchant, id: 3)
-    create(:invoice, id: 11, status: "shipped", customer_id: 3, merchant_id: 3)
-    create(:invoice, id: 12, status: "pending", customer_id: 3, merchant_id: 3)
-
     get "/api/v1/invoices/find", params: {status: "pending"}
 
     invoice = JSON.parse(response.body)
 
     expect(response).to be_success
     expect(invoice["id"]).to eq(12)
+  end
+
+  it "can find one invoice by created_at" do
+    get "/api/v1/invoices/find", params: {created_at: "2018-03-12T17:10:49+00:00"}
+
+    invoice = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice["id"]).to eq(11)
+  end
+
+  it "can find one invoice by updated_at" do
+    get "/api/v1/invoices/find", params: {updated_at: "2018-03-13T17:10:49+00:00"}
+
+    invoice = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice["id"]).to eq(11)
   end
 end
