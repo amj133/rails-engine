d0f69ce25f11ca1b48ec88c432238dd215f4b79f
amj133 <amj@vt.edu>
2018-03-14 12:15:42 -0600

    updated items controller to include before action price_to_cents

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::Items::SearchController < ApplicationController
+  before_action :price_to_cents, only: [:show, :index]
 
   def index
     render json: Item.where(item_params)
@@ -14,6 +15,10 @@ class Api::V1::Items::SearchController < ApplicationController
     params.permit(:id, :name, :description,
                   :unit_price, :merchant_id,
                   :created_at, :updated_at)
+  end
+
+  def price_to_cents
+    params["unit_price"] = item_params["unit_price"].to_f * 100 unless params["unit_price"].nil?
   end
 
 end
