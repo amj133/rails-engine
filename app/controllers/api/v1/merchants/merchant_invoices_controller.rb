40c4a53dfdcd7718dc48e7f795f016828da87076
Joe Jobes <jmrjobes@gmail.com>
2018-03-14 17:28:26 -0600

    Delete extraneous serializers

--- app/controllers/api/v1/merchants/merchant_invoices_controller.rb
+++ app/controllers/api/v1/merchants/merchant_invoices_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Merchants::MerchantInvoicesController < ApplicationController
 
   def index
     merchant = Merchant.find(params[:merchant_id])
-    render json: merchant.invoices, each_serializer: SimpleInvoiceSerializer
+    render json: merchant.invoices
   end
 
 end
