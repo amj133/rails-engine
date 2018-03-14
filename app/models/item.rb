281e9b86fcdcf0e86f72cfa5847a9308c667294d
amj133 <amj@vt.edu>
2018-03-14 10:47:17 -0600

    create formatted_price instance method for item class and add to item spec - passing

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
   validates_presence_of :name, :description, :unit_price, :created_at, :updated_at
+
+  def formatted_price
+    (unit_price / 100.0).round(2)
+  end
 end
