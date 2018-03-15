dbe7fe750d388d521104e4aa9cb4b7c4ae8450d8
amj133 <amj@vt.edu>
2018-03-15 07:27:22 -0600

    add returns collection of transactions for customer test block to customer relations endpoints spec

--- spec/requests/api/v1/customers/customer_relation_endpoints_spec.rb
+++ spec/requests/api/v1/customers/customer_relation_endpoints_spec.rb
@@ -4,7 +4,9 @@ describe "Relationship endpoints for customers" do
   before(:all) do
     customer = create(:customer)
     merchant = create(:merchant)
-    create_list(:invoice, 2, customer: customer, merchant: merchant)
+    invoice_1 = create(:invoice, customer: customer, merchant: merchant)
+    invoice_2 = create(:invoice, customer: customer, merchant: merchant)
+    create_list(:transaction, 2, invoice: invoice_1)
   end
 
   it "returns a collection of invoices for given customer" do
@@ -16,5 +18,16 @@ describe "Relationship endpoints for customers" do
     expect(invoices.count).to eq(2)
     expect(invoices.first["id"]).to eq(1)
     expect(invoices.last["id"]).to eq(2)
+  end
+
+  it "returns a collection of transactions for given customer" do
+    get "/api/v1/customers/1/transactions"
+
+    transactions = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(transactions.count).to eq(2)
+    expect(transactions.first["id"]).to eq(1)
+    expect(transactions.last["id"]).to eq(2)
   end
 end
