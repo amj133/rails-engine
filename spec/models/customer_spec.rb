3b80419aaaa1612f50dfee37bcd4e0b67af6ac03
Joe Jobes <jmrjobes@gmail.com>
2018-03-15 10:22:34 -0600

    Add instance method spec for #favorite_merchant in customers model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe Customer, type: :model do
   describe "Relationships" do
     it {should have_many(:invoices)}
   end
+
+  describe "Instance Methods" do
+    before(:each) do
+      @favorite_merchant = create(:merchant)
+      regular_merchant = create(:merchant)
+      @customer = create(:customer)
+      invoice = create(:invoice, merchant: @favorite_merchant, customer: @customer)
+      second_invoice = create(:invoice, merchant: regular_merchant, customer: @customer)
+      create_list(:transaction, 5, invoice: invoice)
+      create(:transaction, invoice: second_invoice)
+    end
+    describe "#favorite_merchant" do
+      it "should return favorite merchant" do
+        expect(@customer.favorite_merchant).to eq(@favorite_merchant)
+      end
+    end
+  end
 end
