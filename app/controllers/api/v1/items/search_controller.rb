013f80c03b495f24e9ca089fbdc375caa6662803
amj133 <amj@vt.edu>
2018-03-15 18:08:04 -0600

    create invoice items controller

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::Items::SearchController < ApplicationController
   end
 
   def show
+    binding.pry
     render json: Item.find_by(item_params)
   end
 
@@ -18,7 +19,7 @@ class Api::V1::Items::SearchController < ApplicationController
   end
 
   def price_to_cents
-    params["unit_price"] = item_params["unit_price"].to_f * 100 unless params["unit_price"].nil?
+    params["unit_price"] = (item_params["unit_price"].to_f * 100).round(2) unless params["unit_price"].nil?
   end
 
 end
